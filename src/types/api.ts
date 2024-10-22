ecd3af7689f2b9cb73e4b0edf457c60e2b4d3fe3
Rony Bonilla <rbonilla@team.solucionesroots.com>
2024-10-22 14:48:25 -0600

    fix types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,91 +1,111 @@
 export interface APISpaceXResponse {
     docs:          Doc[];
-    totalDocs:     number;
-    offset:        number;
+    hasNextPage:   boolean;
+    hasPrevPage:   boolean;
     limit:         number;
-    totalPages:    number;
+    nextPage:      number;
+    offset:        number;
     page:          number;
     pagingCounter: number;
-    hasPrevPage:   boolean;
-    hasNextPage:   boolean;
     prevPage:      null;
-    nextPage:      number;
-}
-
-export interface Doc {
-    fairings:              Links[] | null;
-    links:                 Links[];
-    static_fire_date_utc:  Date | null;
-    static_fire_date_unix: number | null;
-    net:                   boolean;
-    window:                number;
-    rocket:                Rocket;
-    success:               boolean;
-    failures:              Capsule[];
-    details:               null | string;
+    totalDocs:     number;
+    totalPages:    number;
+   }
+   
+   export interface Doc {
+    auto_update:           boolean;
+    capsules:              string[];
+    cores:                 Core[];
     crew:                  any[];
-    ships:                 Capsule[];
-    capsules:              Capsule[];
-    payloads:              Capsule[];
-    launchpad:             Launchpad;
-    flight_number:         number;
-    name:                  string;
-    date_utc:              Date;
-    date_unix:             number;
     date_local:            Date;
     date_precision:        DatePrecision;
-    upcoming:              boolean;
-    cores:                 Capsule[];
-    auto_update:           boolean;
-    tbd:                   boolean;
-    launch_library_id:     null;
+    date_unix:             number;
+    date_utc:              Date;
+    details:               null | string;
+    failures:              Failure[];
+    fairings:              Fairings | null;
+    flight_number:         number;
     id:                    string;
-}
-
-export enum Capsule {
-    Array = "Array",
-}
-
-export enum DatePrecision {
+    launch_library_id:     null;
+    launchpad:             Launchpad;
+    links:                 Links;
+    name:                  string;
+    net:                   boolean;
+    payloads:              string[];
+    rocket:                Rocket;
+    ships:                 string[];
+    static_fire_date_unix: number | null;
+    static_fire_date_utc:  Date | null;
+    success:               boolean;
+    tbd:                   boolean;
+    upcoming:              boolean;
+    window:                number;
+   }
+   
+   export interface Core {
+    core:            string;
+    flight:          number;
+    gridfins:        boolean;
+    landing_attempt: boolean;
+    landing_success: null;
+    landing_type:    null;
+    landpad:         null;
+    legs:            boolean;
+    reused:          boolean;
+   }
+   
+   export enum DatePrecision {
     Hour = "hour",
-}
-
-export interface Links {
-    patch:      Patch;
-    reddit:     Reddit;
-    flickr:     Flickr;
-    presskit:   null;
-    webcast:    string;
-    youtube_id: string;
+   }
+   
+   export interface Failure {
+    altitude: number | null;
+    reason:   string;
+    time:     number;
+   }
+   
+   export interface Fairings {
+    recovered:        boolean | null;
+    recovery_attempt: boolean | null;
+    reused:           boolean | null;
+    ships:            any[];
+   }
+   
+   export enum Launchpad {
+    The5E9E4501F509094Ba4566F84 = "5e9e4501f509094ba4566f84",
+    The5E9E4502F5090995De566F86 = "5e9e4502f5090995de566f86",
+   }
+   
+   export interface Links {
     article:    string;
+    flickr:     Flickr;
+    patch:      Patch;
+    presskit:   null | string;
+    reddit:     Reddit;
+    webcast:    string;
     wikipedia:  string;
-}
-
-export interface Flickr {
-    small:    any[];
+    youtube_id: string;
+   }
+   
+   export interface Flickr {
     original: any[];
-}
-
-export interface Patch {
-    small: string;
+    small:    any[];
+   }
+   
+   export interface Patch {
     large: string;
-}
-
-export interface Reddit {
+    small: string;
+   }
+   
+   export interface Reddit {
     campaign: null;
-    launch:   null;
+    launch:   null | string;
     media:    null;
     recovery: null;
-}
-
-
-export enum Launchpad {
-    The5E9E4501F509094Ba4566F84 = "5e9e4501f509094ba4566f84",
-    The5E9E4502F509092B78566F87 = "5e9e4502f509092b78566f87",
-    The5E9E4502F5090995De566F86 = "5e9e4502f5090995de566f86",
-}
-
-export enum Rocket {
+   }
+   
+   export enum Rocket {
     The5E9D0D95Eda69955F709D1Eb = "5e9d0d95eda69955f709d1eb",
     The5E9D0D95Eda69973A809D1Ec = "5e9d0d95eda69973a809d1ec",
-}
+   }
+   
